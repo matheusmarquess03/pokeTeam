fc8f16a6996d08bf9c6a13cdaa82fcdfcd330ce5
Matheus Marques <matheus.marques@leadstation.com.br>
2021-09-05 22:30:13 -0300

    create service that consumes api

---
+++ app/services/get_pokedex.rb
@@ -0,0 +1,23 @@
+require 'rest-client'
+require 'json'
+
+class GetPokedex
+  attr_reader :pokemon
+
+  def initialize(pokemon="")
+    @pokemon = "#{pokemon}/"
+    @url = "https://pokeapi.co/api/v2/pokemon/#{@pokemon}?limit=1118"
+  end
+
+  def get_pokedex
+    resp = RestClient.get(@url)
+    resp = JSON.parse(resp.body)['results']
+    resp.sort_by { |h| h['name'] }
+  end
+
+  def get_pokemon()
+    resp = RestClient.get(@url)
+    resp = JSON.parse(resp.body)
+    resp
+  end
+end
